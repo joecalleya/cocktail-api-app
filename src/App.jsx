04bbf9f294b65fc08f225bfb111939b3fb0da276
Joe Calleya <jcalleya@gmail.com>
2020-10-27 17:09:31 +0000

    stable filter functrionality

--- src/App.jsx
+++ src/App.jsx
@@ -6,86 +6,79 @@ import library from "./data/fa-library";
 
 let preFiltered  = [];
 
-
 const App = () => {
 
-  const [searchResult , setsearchResult] = useState('');
-  
-  // const [martiniFilterOnOff,setMartiniFilterOnOff] = useState(false);
-
-
+  // 1. At the top let's define our variables
   let martiniFilter = false;
-  let filteredResults = [];
 
+  // 2. then let's define the variable we are keeping in state
+  const [searchResult , setSearchResult] = useState('');
+  const [filteredResults, setFilteredResults] = useState([]);
+
+  // 3. Put useEffect here for onload events
+  
+  useEffect(() => {
+    getApiData("");
+  }, []);
+  useEffect(() => {
+    if (searchResult) init()
+  }, [searchResult])
+
+  // 3. Put our functions together
   const getApiData = (searchWords) => {
     fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchWords}`)
       .then ((res) => res.json())
       .then ((res) => {
         const rawData = res.drinks
-        setsearchResult(rawData)
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+        setSearchResult(rawData);        
+      })
+      .catch((err) => {
+        console.log(err);
+      });
     };
 
-      useEffect(() => {
-    getApiData("");
-  }, []);
+  const toggleFilter = (item) => {
+    return item = !item;
+  }
 
-    const toggleFilter = (item) => {
-      return item = !item;
+  const init = () => {
+    // 1. If we have a search result, set prefiltered to that search result
+    if (searchResult.length >= 1 && preFiltered.length == 0 ) {
+      preFiltered = searchResult
     }
 
-  // set pre-filtered on first run.
+    // 2. Create some filter parameters, to show as checkboxes on the page
+    let filterParameters = [{name:"Martini Filter", toggle: true , columnUsed: 'strGlass'  ,  value: "Cocktail glass"}, {name:"Tumbler Filter", toggle: false ,columnUsed: 'strGlass' , value: "Highball Glass"}];
 
-  if (searchResult.length >= 1 && preFiltered.length == 0 ) {preFiltered = searchResult}
-
-   console.log("prefilted rows",preFiltered.length)
-
-// }
-// define filter function for filtring drinks types - glass 
-const filterDrinks = (type,value,filterOnOrOff) => {
-    if (searchResult.length >= 1) {
-        if (type == "GLASS" && filterOnOrOff) 
-          {
-            filteredResults =  preFiltered.filter((drink) => drink.strGlass === value)
-            console.log('do filter',filteredResults.length)
-          }
-        else if (type == "GLASS" && !filterOnOrOff) 
-          {
-          filteredResults =  preFiltered.filter((drink) => drink.strGlass !== value)
-          console.log('Undoo filter',filterOnOrOff,filteredResults.length)
-
-          }
-          setsearchResult(filteredResults)
-    }
-
-      };
-
-      const clearFilter = () => 
-      {
-        getApiData("");
-
+    // 3. Define a filter function for filtring drinks using the filter array 
+    const filterResults = [];
+    filterParameters.forEach(parameter => {
+      if (parameter.toggle == true) {
+        // This filter is active, let's filter the results by it
+        filterResults.push(
+          preFiltered.filter(cockTail => cockTail[parameter.columnUsed] && cockTail[parameter.columnUsed] == parameter.value)
+        );
       }
+    })
 
+    // 4. We have our filtered cocktails, save them in state so the page re-renders
+    setFilteredResults(filterResults);
+  }
 
-
- return (
+  // 5. Our return function comes last with nice indents
+  return (
    <div className={styles.appContainer}>
-   <div className="dash">
-      <Dashboard
-      getApiData={getApiData}
-      searchResult={searchResult}
-      filterDrinks={filterDrinks}
-      toggleFilter={toggleFilter}
-      martiniFilter={martiniFilter}
-      clearFilter={clearFilter}
-
-      />
+      <div className="dash">
+          <Dashboard
+            getApiData={getApiData}
+            searchResult={searchResult}
+            filterDrinks={filteredResults}
+            toggleFilter={toggleFilter}
+            martiniFilter={martiniFilter}
+            // clearFilter={clearFilter}
+          />
+        </div>
     </div>
-
-  </div>
   );
 }
 
--- src/components/DrinkCard/DrinkCard.jsx
+++ src/components/DrinkCard/DrinkCard.jsx
@@ -3,13 +3,13 @@ import styles from './DrinkCard.module.scss'
 
  const DrinkCard = (props) => {
 
-    const {idDrink, strDrink, strDrinkAlternate, strDrinkES, strDrinkDE, strDrinkFR, 
+   const {idDrink, strDrink, strDrinkAlternate, strDrinkES, strDrinkDE, strDrinkFR, 
         strDrinkThumb,  strTags, strVideo, strCategory, strIBA, strAlcoholic, strGlass, strInstructions, 
         strIngredient1, strIngredient2, strIngredient3, strIngredient4, strIngredient5, strIngredient6, strIngredient7, strIngredient8, 
         strIngredient9, strIngredient10, strIngredient11, strIngredient12, strIngredient13, strIngredient14, strIngredient15, strMeasure1, strMeasure2, 
         strMeasure3, strMeasure4, strMeasure5, strMeasure6, strMeasure7, strMeasure8, strMeasure9, strMeasure10, 
         strMeasure11, strMeasure12, strMeasure13, strMeasure14, strMeasure15, strCreativeCommonsConfirmed, dateModified } = props.searchResult;
-
+ 
     return (
         <div className={styles.resultCard}>
                 <div className={styles.image}>
